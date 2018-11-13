5a94e33bd0d6ec2ce75b42ee520f28814abf881e
aisilu93 <aisilu93@yandex.ru>
2018-11-13 17:04:00 +0300

    Added tests for Remove() method

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -25,19 +25,19 @@ namespace test1
             _list.Add(2);
             _list.Add(3);
             _list.Add(4);
-            Assert.AreEqual("1234", _list.ToStr());
+            Assert.AreEqual("1234", _list.ToString());
         }
         [TestMethod]
         public void TestAdd0()
         {
-            Assert.AreEqual("", _list.ToStr());
+            Assert.AreEqual("", _list.ToString());
         }
         [TestMethod]
         public void TestAdd2()
         {
             _list.Add(1);
             _list.Add(2);
-            Assert.AreEqual("12", _list.ToStr());
+            Assert.AreEqual("12", _list.ToString());
         }
         [TestMethod]
         public void TestClearNotEmpty()
@@ -49,13 +49,13 @@ namespace test1
             _list.Add(1);
             _list.Add(2);
             _list.Clear();
-            Assert.AreEqual("", _list.ToStr());
+            Assert.AreEqual("", _list.ToString());
         }
         [TestMethod]
         public void TestClearEmpty()
         {
             _list.Clear();
-            Assert.AreEqual("", _list.ToStr());
+            Assert.AreEqual("", _list.ToString());
         }
         [TestMethod]
         public void TestIndexOf()
@@ -76,7 +76,7 @@ namespace test1
             _list.Add(3);
             _list.Add(1234);
             _list.Insert(2, 54);
-            Assert.AreEqual("125431234", _list.ToStr());
+            Assert.AreEqual("125431234", _list.ToString());
         }
         [TestMethod]
         public void TestInsert_to_back()
@@ -86,7 +86,7 @@ namespace test1
             _list.Add(3);
             _list.Add(1234);
             _list.Insert(_list.Count, 54);
-            Assert.AreEqual("123123454", _list.ToStr());
+            Assert.AreEqual("123123454", _list.ToString());
         }
         [TestMethod]
         public void TestRemoveAt_first()
@@ -96,7 +96,7 @@ namespace test1
             _list.Add(3);
             _list.Add(1234);
             _list.RemoveAt(0);
-            Assert.AreEqual("231234", _list.ToStr());
+            Assert.AreEqual("231234", _list.ToString());
         }
         [TestMethod]
         public void TestRemoveAt_middle()
@@ -106,7 +106,7 @@ namespace test1
             _list.Add(3);
             _list.Add(1234);
             _list.RemoveAt(1);
-            Assert.AreEqual("131234", _list.ToStr());
+            Assert.AreEqual("131234", _list.ToString());
         }
         [TestMethod]
         public void TestRemoveAt_negative()
@@ -115,7 +115,7 @@ namespace test1
             _list.Add(2);
             _list.Add(3);
             _list.Add(1234);
-            Assert.AreEqual("1231234", _list.ToStr());
+            Assert.AreEqual("1231234", _list.ToString());
         }
         [TestMethod]
         public void TestRemove()
@@ -125,7 +125,7 @@ namespace test1
             _list.Add(3);
             _list.Add(1234);
             _list.Remove(3);
-            Assert.AreEqual("121234", _list.ToStr());
+            Assert.AreEqual("121234", _list.ToString());
         }
         [TestMethod]
         public void TestRemove_not_exists()
@@ -135,7 +135,30 @@ namespace test1
             _list.Add(3);
             _list.Add(1234);
             _list.Remove(30);
-            Assert.AreEqual("1231234", _list.ToStr());
+            Assert.AreEqual("1231234", _list.ToString());
+        }
+        [TestMethod]
+        public void TestRemove_return_true()
+        {
+            _list.Add(1);
+            _list.Add(2);
+            _list.Add(3);
+            _list.Add(1234);
+            Assert.AreEqual(true,_list.Remove(3));
+        }
+        [TestMethod]
+        public void TestRemove_return_false()
+        {
+            Assert.AreEqual(false, _list.Remove(3));
+        }
+        [TestMethod]
+        public void TestRemove_return_Not_contains()
+        {
+            _list.Add(1);
+            _list.Add(2);
+            _list.Add(3);
+            _list.Add(1234);
+            Assert.AreEqual(false, _list.Remove(5));
         }
         [TestMethod]
         public void TestContains()
@@ -169,7 +192,7 @@ namespace test1
             {
                 actual += a.ToString();
             }
-            String expected = _list.ToStr();
+            String expected = _list.ToString();
             Assert.AreEqual(actual, expected);
         }
         [TestMethod]
